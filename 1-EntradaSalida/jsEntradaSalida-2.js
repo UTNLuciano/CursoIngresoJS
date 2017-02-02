79e23158fdbfd02b72290ab3c442ad542fb393f3
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2017-02-02 20:23:01 -0300

    Primer commit
    Hicimos el ejercicio 1 y 2 en clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,14 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	Nombre = prompt("ingrese su nombre");
+	alert(Nombre);
+
+
+
+
+
+	var person = prompt("Ingresa tu nombre", "Natalia Natalia");
+
 }
 
