a3424ac8b0589debb29f537c96b2ad4a2685a997
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2017-02-03 22:05:38 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,50 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var resultado;
+
+	largoDelTerreno=document.getElementById('Largo').value;
+	anchoDelTerreno=document.getElementById('Ancho').value;
+	// radioDelTerreno=document.getElementById('Radio').value;
+	largoDelTerreno=parseInt(largoDelTerreno);
+	anchoDelTerreno=parseInt(anchoDelTerreno);
+	// radioDelTerreno=parseInt(radioDelTerreno);
+	resultado=(((largoDelTerreno+anchoDelTerreno)*2)*3);// +radioDelTerreno);
+	alert(resultado);
 
 }
 function Circulo () 
 {
-	
+
+	var radio;
+	var resultado;
+
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+	resultado=((radio*2*3.14)*3);
+	alert(resultado);
+
 }
 function Materiales () 
 {
+
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var resultadoUno;
+	var resultadoDos;
+
+	largoDelTerreno=document.getElementById('Largo').value;
+	anchoDelTerreno=document.getElementById('Ancho').value;
+	
+	largoDelTerreno=parseInt(largoDelTerreno);
+	anchoDelTerreno=parseInt(anchoDelTerreno);
+
+	resultadoUno=((largoDelTerreno+anchoDelTerreno)*2);
+	resultadoDos=((largoDelTerreno+anchoDelTerreno)*3);
+	
+	alert("Bolsas de cemento: "+resultadoUno);
+	alert("Bolsas de cal: "+resultadoDos);
 	
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,39 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var productoUno;
+	var productoDos;
+	var productoTres;
+	var resultado;
+
+	productoUno=document.getElementById('PrecioUno').value;
+	productoDos=document.getElementById('PrecioDos').value;
+	productoTres=document.getElementById('PrecioTres').value;
+	productoUno=parseInt(productoUno);
+	productoDos=parseInt(productoDos);
+	productoTres=parseInt(productoTres);
+	resultado=(productoUno+productoDos+productoTres);
+	alert(resultado);
 }
 function Promedio () 
 {
-	
+	productoUno=document.getElementById('PrecioUno').value;
+	productoDos=document.getElementById('PrecioDos').value;
+	productoTres=document.getElementById('PrecioTres').value;
+	productoUno=parseInt(productoUno);
+	productoDos=parseInt(productoDos);
+	productoTres=parseInt(productoTres);
+	resultado=((productoUno+productoDos+productoTres)/3);
+	alert(resultado);
 }
 function PrecioFinal () 
 {
-	
+	productoUno=document.getElementById('PrecioUno').value;
+	productoDos=document.getElementById('PrecioDos').value;
+	productoTres=document.getElementById('PrecioTres').value;
+	productoUno=parseInt(productoUno);
+	productoDos=parseInt(productoDos);
+	productoTres=parseInt(productoTres);
+	resultado=((productoUno+productoDos+productoTres)*1.21);
+	alert(resultado);
 }
