8d02cac304eaf5ebca1670acda96c3858b25c921
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2017-02-10 21:12:01 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,9 +4,23 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+		switch (mesDelAño)
+	{
+		case"Febrero":
+			alert("Tiene 28 días");
+			break;
+		default:
+			alert("Este mes tiene 30 días");
+				break;
+		case"Enero":
+		case"Marzo":
+		case"Mayo":
+		case"Julio":
+		case"Septiembre":
+		case"Diciembre":
+			alert("Este mes tiene 31 dias")
+				break;
+	}//FIN DE LA FUNCIÓN
 	
-	
-
-
 
 }//FIN DE LA FUNCIÓN
