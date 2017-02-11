c85d2bd4082a90696943016dd5051446d5f34cbd
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2017-02-10 21:12:38 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -4,9 +4,10 @@ function Mostrar()
 
 	var edad;
 	var estado;
+
 	edad=document.getElementById('edad').value;
 	estado=document.getElementById("estadoCivil").value;
-	if (edad<18)	
+		if (edad<18&&estado!="Soltero")	
 	{
 		alert("Es muy pequeño para NO ser soltero");
 	}
